30b2ab40d2436887bb29c52faab4af5f8affa3e7
rutvik110 <takrutvik@gmail.com>
2022-06-01 15:40:12 +0530

    update

--- TipCalculator_Materials/starter/lib/presentation/tip_calculator_view.dart
+++ TipCalculator_Materials/starter/lib/presentation/tip_calculator_view.dart
@@ -17,52 +17,7 @@ class _TipCalculatorViewState extends State<TipCalculatorView> {
       appBar: AppBar(
         title: const Text('Tip Calculator'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const TextField(), // For Bill Amount
-
-            const SizedBox(
-              height: 20,
-            ),
-            const TextField(), // For Tip Percentage
-
-            const Divider(
-              height: 30,
-            ),
-
-            Row(
-              children: [
-                const Text(
-                  'Total Tip: ',
-                  // TODO : add title TextStyle
-                ),
-                const Text(
-                  '10\$',
-                  // TODO : add amount TextStyle
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: [
-                const Text(
-                  'Total Bill: ',
-                  // TODO : add title TextStyle
-                ),
-                const Text(
-                  '20\$',
-                  // TODO : add amount TextStyle
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      body: Container(),
     );
   }
 }
